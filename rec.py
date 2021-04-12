7c03d3358be307267c49b2ecb2b13e5731f68b30
flowitoo <69961128+flowitoo@users.noreply.github.com>
2021-04-12 02:32:18 +0200

    Update rec.py

--- rec.py
+++ rec.py
@@ -6,19 +6,29 @@
 # Taken and modified from https://docs.python.org/3/library/socket.html
 import socket
 import time
+import sys
 HOST = ''
 PORT = 50007
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(25000)
-            if(data):
-                print(data.decode("ISO-8859-1"))
-                with open(f"{int(time.time())}.ldb","wb") as f:
-                    f.write(data)
-                    f.close()
-                s.close()
+while True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((HOST, PORT))
+        s.listen(5)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(50000)
+                if(data):
+                    with open(f"{int(time.time())}.ldb","wb") as f:
+                        f.write(data)
+                        f.close()
+                    print('Recived by', addr)
+                    break
+                else:
+                     break
+            conn.close()
+        s.close()
+    except KeyboardInterrupt:
+        break
+sys.exit()
